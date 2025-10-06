99d28456360a6c1564f14fc1c65c8c366dc94657
georgehaan08 <03george.haan@kamsc.org>
2025-10-06 14:10:52 -0400

    Update TempConverter.java

--- src/main/java/TempConverter.java
+++ src/main/java/TempConverter.java
@@ -1,46 +1,51 @@
-
 import java.util.Scanner;
 
-public class TempConverter
-{
+public class TempConverter {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        Boolean status = true;
-        Double cel, fah;
-        while (status)
+        boolean status = true;
+        int choice;
+
+        while(status)
         {
-            System.out.println("\n\n\nChoose an option:");
-            System.out.println("1: Fahrenheit to Celsius");
-            System.out.println("2: Celsius to Fahrenheit");
-            System.out.println("3: Exit");
-            System.out.print("Enter choice: ");
-
-            int choice = sc.nextInt();
-
-            switch (choice)
+            System.out.println("\n\nMake a selection ");
+            System.out.println("1, Fahrenheit to Celsius ");
+            System.out.println("2, Celsius to Fahrenheit ");
+            System.out.println("3, Exit ");
+            choice = sc.nextInt();
+            switch(choice)
             {
                 case 1:
-                    System.out.print("Enter degrees fahrenheit: ");
-                    fah = sc.nextDouble();
-                    cel = (fah - 32) * 5 / 9;
-                    System.out.println(cel);
+                {
+                    System.out.println("Enter degrees fahrenheit: ");
+                    double fah = sc.nextDouble();
+                    double old = fah;
+                    fah -= 32;
+                    fah *= (double) 5/9;
+                    System.out.println("Conversion Results: ");
+                    System.out.println("Fahrenheit: " + old);
+                    System.out.println("Celsius: " + fah);
                     break;
-
+                }
                 case 2:
-                    System.out.print("Enter degrees celsius: ");
-                    cel = sc.nextDouble();
-                    fah = (cel * 9 / 5) + 32;
-                    System.out.println(fah);
+                {
+                    System.out.println("Enter degrees celsius: ");
+                    double cel = sc.nextDouble();
+                    double old = cel;
+                    cel *= (double) 9/5;
+                    cel += 32;
+                    System.out.println("Conversion Results: ");
+                    System.out.println("Celsius: " + old);
+                    System.out.println("Fahrenheit: " + cel);
                     break;
-
+                }
                 case 3:
-                    status = false;
+                {
                     System.out.println("Goodbye!");
+                    status = false;
                     break;
-
-                default:
-                    System.out.println("Enter 1, 2, or 3");
+                }
             }
         }
     }
